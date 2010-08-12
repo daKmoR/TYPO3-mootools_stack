177ac57c83bf8925609d696c5d25251b2e97ec72
TYPO3 Demo Installation <demo@clients.webteam.at>
2010-08-13 01:35:18 +0200

    TER v0.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "mootools_core".
+# Extension Manager/Repository config file for ext "mootools_stack".
 #
-# Auto generated 17-06-2010 15:56
+# Auto generated 13-08-2010 01:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"947b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"9e81";s:20:"doc/wizard_form.html";s:4:"ef7d";}',
+	'_md5_values_when_last_written' => 'a:49:{s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"ebd2";s:15:"res/package.yml";s:4:"28b9";s:30:"res/Demos/Overlay/Overlay.html";s:4:"fbb4";s:37:"res/Docs/Galleries/FlexBox/FlexBox.md";s:4:"d545";s:50:"res/Docs/Galleries/FlexSlide/FlexSlide.Advanced.md";s:4:"c39a";s:53:"res/Docs/Galleries/FlexSlide/FlexSlide.Transitions.md";s:4:"35c3";s:41:"res/Docs/Galleries/FlexSlide/FlexSlide.md";s:4:"e493";s:68:"res/Settings/Galleries/FlexBox/LightBox/FlexBox.Setting.LightBox.css";s:4:"c9a8";s:67:"res/Settings/Galleries/FlexBox/LightBox/FlexBox.Setting.LightBox.js";s:4:"3b39";s:56:"res/Settings/Galleries/FlexBox/LightBox/images/close.gif";s:4:"0e54";s:58:"res/Settings/Galleries/FlexBox/LightBox/images/loading.gif";s:4:"7e99";s:55:"res/Settings/Galleries/FlexBox/LightBox/images/next.gif";s:4:"485d";s:59:"res/Settings/Galleries/FlexBox/LightBox/images/previous.gif";s:4:"d935";s:66:"res/Settings/Galleries/FlexBox/Minimal/FlexBox.Setting.Minimal.css";s:4:"db42";s:70:"res/Settings/Galleries/FlexSlide/Minimal/FlexSlide.Setting.Minimal.css";s:4:"5435";s:58:"res/Settings/Galleries/FlexSlide/Minimal/images/loader.gif";s:4:"9344";s:68:"res/Settings/Galleries/FlexSlide/Simple/FlexSlide.Setting.Simple.css";s:4:"49c4";s:67:"res/Settings/Galleries/FlexSlide/Simple/FlexSlide.Setting.Simple.js";s:4:"f54c";s:64:"res/Settings/Galleries/FlexSlide/Simple/images/next_previous.gif";s:4:"606b";s:76:"res/Settings/Galleries/FlexSlide/SlideShow2/FlexSlide.Setting.SlideShow2.css";s:4:"7dc0";s:75:"res/Settings/Galleries/FlexSlide/SlideShow2/FlexSlide.Setting.SlideShow2.js";s:4:"ba26";s:71:"res/Settings/Galleries/FlexSlide/SlideShow2/images/controller-first.gif";s:4:"f421";s:70:"res/Settings/Galleries/FlexSlide/SlideShow2/images/controller-last.gif";s:4:"11ea";s:70:"res/Settings/Galleries/FlexSlide/SlideShow2/images/controller-next.gif";s:4:"2eec";s:71:"res/Settings/Galleries/FlexSlide/SlideShow2/images/controller-pause.gif";s:4:"afd4";s:70:"res/Settings/Galleries/FlexSlide/SlideShow2/images/controller-prev.gif";s:4:"8965";s:65:"res/Settings/Galleries/FlexSlide/SlideShow2/images/controller.png";s:4:"6873";s:64:"res/Settings/Galleries/FlexSlide/SlideShow2/images/slideshow.gif";s:4:"caa9";s:67:"res/Settings/Galleries/FlexSlide/SlideShow2/images/thumbnails-a.png";s:4:"c9ad";s:67:"res/Settings/Galleries/FlexSlide/SlideShow2/images/thumbnails-b.png";s:4:"8f1d";s:82:"res/Settings/Menus/MenuMatic/DefaultHorizontal/MenuMatic.DefaultHorizontal-ie6.css";s:4:"300a";s:78:"res/Settings/Menus/MenuMatic/DefaultHorizontal/MenuMatic.DefaultHorizontal.css";s:4:"3da3";s:68:"res/Settings/Menus/MenuMatic/DefaultHorizontal/images/arrow_down.gif";s:4:"10be";s:73:"res/Settings/Menus/MenuMatic/DefaultHorizontal/images/arrow_down_over.gif";s:4:"0bf8";s:69:"res/Settings/Menus/MenuMatic/DefaultHorizontal/images/arrow_right.gif";s:4:"5531";s:74:"res/Settings/Menus/MenuMatic/DefaultHorizontal/images/arrow_right_over.gif";s:4:"b2ad";s:63:"res/Settings/Visual/Overlay/Default/Overlay.Setting.Default.css";s:4:"38e7";s:34:"res/Source/Class/Class.Settings.js";s:4:"8be0";s:57:"res/Source/Element/Elements.equalize/Elements.equalize.js";s:4:"b99c";s:39:"res/Source/Galleries/FlexBox/FlexBox.js";s:4:"3e7e";s:41:"res/Source/Galleries/FlexBox/FlexBoxes.js";s:4:"f96c";s:52:"res/Source/Galleries/FlexSlide/FlexSlide.Advanced.js";s:4:"5ff8";s:55:"res/Source/Galleries/FlexSlide/FlexSlide.Transitions.js";s:4:"0489";s:43:"res/Source/Galleries/FlexSlide/FlexSlide.js";s:4:"1ce3";s:49:"res/Source/Galleries/FlexSlide/FlexSlides.Auto.js";s:4:"ba34";s:44:"res/Source/Galleries/FlexSlide/FlexSlides.js";s:4:"55eb";s:29:"res/Source/Menus/MenuMatic.js";s:4:"d494";s:36:"res/Source/Visual/Overlay/Overlay.js";s:4:"13c1";}',
+	'suggests' => array(
+	),
 );
 
 ?>
