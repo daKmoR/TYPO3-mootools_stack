5accbceb8678ab2fd0035fc0ede8afd663058abf
daKmoR <thomas.allmer@webteam.at>
2011-06-20 22:23:40 +0200

    updats with behaviors YEAH :) breaks flexbox :/

--- ts/Galleries/FlexSlide/FlexSlide.Minimal.ts
+++ ts/Galleries/FlexSlide/FlexSlide.Minimal.ts
@@ -1,4 +1,5 @@
-page.includeCSS.flexslide = EXT:mootools_stack/res/Settings/Galleries/FlexSlide/Minimal/FlexSlide.Setting.Minimal.css
+page.includeCSS.flexSlideMinimal = EXT:mootools_stack/res/Settings/Galleries/FlexSlide/Minimal/FlexSlide.Setting.Minimal.css
+
 plugin.tx_mootoolspackager_pi1.files := addToList(Stack/FlexSlide, Stack/FlexSlide.Transitions)
 
 plugin.tx_mootoolspackager_pi1.domready {
--- ts/Galleries/FlexSlide/FlexSlide.Tabs.ts
+++ ts/Galleries/FlexSlide/FlexSlide.Tabs.ts
@@ -1,4 +1,6 @@
-page.includeCSS.tabs = EXT:mootools_stack/res/Settings/Galleries/FlexSlide/Tab/FlexSlide.Setting.Tab.css
+page.includeCSS.flexSlideMinimal = EXT:mootools_stack/res/Settings/Galleries/FlexSlide/Minimal/FlexSlide.Setting.Minimal.css
+page.includeCSS.flexSlideTab = EXT:mootools_stack/res/Settings/Galleries/FlexSlide/Tab/FlexSlide.Setting.Tab.css
+
 plugin.tx_mootoolspackager_pi1.files := addToList(Stack/FlexSlide, Stack/FlexSlide.Transitions, Stack/FlexSlide.Setting.Tab)
 
 plugin.tx_mootoolspackager_pi1.domready {
