33e4d9fcc9f0d7b62a8c5f9989b76529c142d12b
daKmoR <thomas.allmer@webteam.at>
2012-07-04 13:00:35 +0200

    [FEATURE] MooDropMenu as Submodule and Behavior in Stack

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,8 +5,10 @@ t3lib_extMgm::addTypoScript($_EXTKEY, 'setup', '
 plugin.tx_mootoolsessentials {
 	settings {
 		manifests {
+			Bootstrap = EXT:mootools_stack/Resources/Public/Manifests/Bootstrap/
 			Stack = EXT:mootools_stack/Resources/Public/Manifests/Stack/
 			MooTools-DatePicker = EXT:mootools_stack/Resources/Public/Manifests/MootoolsDatepicker/
+			MooDropMenu = EXT:mootools_stack/Resources/Public/Manifests/MooDropMenu/
 			Slide = EXT:mootools_stack/Resources/Public/Manifests/Slide/
 			PopUp = EXT:mootools_stack/Resources/Public/Manifests/PopUp/
 			MooGooMaps = EXT:mootools_stack/Resources/Public/Manifests/MooGooMaps/
